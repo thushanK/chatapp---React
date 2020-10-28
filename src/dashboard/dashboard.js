60168a1d4eb43f183cfae00164130b49edba3356
Thushan <ktwimalaweera@gmail.com>
2020-10-28 16:42:17 +0530

    Initial Commit

---
+++ src/dashboard/dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class DashboardComponent extends React.Component {
+    render(){
+        return(<div>Hello world From Dashboard</div>)
+    }
+}
+
+export default DashboardComponent;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import LoginComponent from './login/login';
+import SignupComponent from './signup/signup';
+import DashboardComponent from './dashboard/dashboard';
+
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAisk1CqBxDz91HINwzeFFO2OtJD9V4riM",
+  authDomain: "chatapp-84ccd.firebaseapp.com",
+  databaseURL: "https://chatapp-84ccd.firebaseio.com",
+  projectId: "chatapp-84ccd",
+  storageBucket: "chatapp-84ccd.appspot.com",
+  messagingSenderId: "745955645600",
+  appId: "1:745955645600:web:70ecc30cb10a033fd391c5",
+  measurementId: "G-XG4RPTFJCE"
+});
+
+const routing = (
+  <Router>
+    <div id='routing-container'>
+      <Route path='/login' component={LoginComponent}></Route>
+      <Route path='/signup' component={SignupComponent}></Route>
+      <Route path='/dashboard' component={DashboardComponent}></Route>
+    </div>
+  </Router>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
